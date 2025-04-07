e2ccec5ffc0b4fba0962689b5a73d6be122ffb76
zackledotcom <147879157+zackledotcom@users.noreply.github.com>
2025-04-06 22:05:29 -0500

    Add activepieces/tools/scripts/validate-publishable-packages.ts 1743995129469

---
+++ activepieces/tools/scripts/validate-publishable-packages.ts
@@ -0,0 +1,19 @@
+import { findAllPiecesDirectoryInSource } from './utils/piece-script-utils';
+import { packagePrePublishChecks } from './utils/package-pre-publish-checks';
+
+const main = async () => {
+  const piecesMetadata = await findAllPiecesDirectoryInSource()
+
+  const packages = [
+    ...piecesMetadata,
+    'packages/pieces/community/framework',
+    'packages/shared',
+    'packages/pieces/community/common',
+  ]
+
+  const validationResults = packages.map(p => packagePrePublishChecks(p))
+
+  Promise.all(validationResults);
+}
+
+main();
