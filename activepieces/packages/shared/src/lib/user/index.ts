41a0f5b3c5dea22c500fa585fe3ff76abe5b9712
zackledotcom <147879157+zackledotcom@users.noreply.github.com>
2025-04-06 22:05:57 -0500

    Add activepieces/packages/shared/src/lib/user/index.ts 1743995157418

---
+++ activepieces/packages/shared/src/lib/user/index.ts
@@ -0,0 +1,13 @@
+import { Static, Type } from '@sinclair/typebox'
+import { PlatformRole, UserStatus } from './user'
+
+export * from './user'
+
+export const UpdateUserRequestBody = Type.Object({
+    status: Type.Optional(Type.Enum(UserStatus)),
+    platformRole: Type.Optional(Type.Enum(PlatformRole)),
+    externalId: Type.Optional(Type.String()),
+})
+
+export type UpdateUserRequestBody = Static<typeof UpdateUserRequestBody>
+
