769287b855795350dc7c37dd788326711ed61f39
zackledotcom <147879157+zackledotcom@users.noreply.github.com>
2025-04-06 22:05:36 -0500

    Add activepieces/tools/scripts/pieces/publish-pieces-to-npm.ts 1743995136472

---
+++ activepieces/tools/scripts/pieces/publish-pieces-to-npm.ts
@@ -0,0 +1,15 @@
+import { publishNxProject } from '../utils/publish-nx-project'
+import { findAllPiecesDirectoryInSource } from '../utils/piece-script-utils'
+
+const publishPiece = async (nxProjectPath: string): Promise<void> => {
+  console.info(`[publishPiece] nxProjectPath=${nxProjectPath}`)
+  await publishNxProject(nxProjectPath)
+}
+
+const main = async () => {
+  const piecesSource = await findAllPiecesDirectoryInSource()
+  const publishResults = piecesSource.map(p => publishPiece(p))
+  await Promise.all(publishResults)
+}
+
+main()
