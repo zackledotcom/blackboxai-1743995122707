d1cb25c9d0d74e6292eff78f32b6881f8a262bd9
zackledotcom <147879157+zackledotcom@users.noreply.github.com>
2025-04-06 22:10:29 -0500

    Add activepieces/packages/server/api/src/app/store-entry/store-entry-entity.ts 1743995428867

---
+++ activepieces/packages/server/api/src/app/store-entry/store-entry-entity.ts
@@ -0,0 +1,30 @@
+import { STORE_KEY_MAX_LENGTH, StoreEntry } from '@activepieces/shared'
+import { EntitySchema } from 'typeorm'
+import {
+    ApIdSchema,
+    BaseColumnSchemaPart,
+    JSONB_COLUMN_TYPE,
+} from '../database/database-common'
+
+type StoreEntrySchema = StoreEntry
+
+export const StoreEntryEntity = new EntitySchema<StoreEntrySchema>({
+    name: 'store-entry',
+    columns: {
+        ...BaseColumnSchemaPart,
+        key: {
+            type: String,
+            length: STORE_KEY_MAX_LENGTH,
+        },
+        projectId: ApIdSchema,
+        value: {
+            type: JSONB_COLUMN_TYPE,
+            nullable: true,
+        },
+    },    
+    uniques: [
+        {
+            columns: ['projectId', 'key'],
+        },
+    ],
+})
